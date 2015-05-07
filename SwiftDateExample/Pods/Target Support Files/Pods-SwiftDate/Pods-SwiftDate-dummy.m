d6487d4e62aab043b8c7deb4a4481eb3a5192011
Daniele Margutti <me@danielemargutti.com>
2015-05-07 14:20:54 +0200

    Removed example, preparing for test project

--- SwiftDateExample/Pods/Target Support Files/Pods-SwiftDate/Pods-SwiftDate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftDate : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftDate
-@end
--- SwiftDateExample/Pods/Target Support Files/Pods-SwiftDate/Pods-SwiftDate-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SwiftDateVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftDateVersionString[];
-

1ab887330164f6d55759f6c77f62357ab7c05370
chenyangcun <ycchen@portaura.com>
2015-07-28 00:07:15 +0800

    support swift 2.0

--- ExampleProject/ExampleProject/ViewController.swift
+++ ExampleProject/ExampleProject/ViewController.swift
@@ -14,10 +14,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-		let date = "2015-06-17T09:10:00.000Z".toDate(format: DateFormat.ISO8601)
-		var d = NSDate()-1.hour
-		var abb = d.toRelativeString(abbreviated: true, maxUnits: 3)
-		println("data: \(abb)")
+        let date = NSDate()
+        
+        _ = date.toString(format: DateFormat.Custom("YYYY-MM-DD"))
+
+        
+        _ = date+1.day
+        let two_months_ago = date-2.months
+        print(two_months_ago.toLongDateString())
+        
+        
+        
+        let date_as_utc = date.toUTC() //UTC 时间
+        let date_as_beijing = date_as_utc.toTimezone("GMT+0800") //北京时间
+        
+        print(date_as_utc.toLongTimeString())
+        print(date_as_beijing?.toLongTimeString())
+        
+		let d = NSDate()-1.hour
+        
+        print(d.year)
+        
+        
+        let date1 = NSDate.date(fromString: "2015-07-26", format: DateFormat.Custom("YYYY-MM-DD"))
+        let date2 = NSDate.date(fromString: "2015-07-27", format: DateFormat.Custom("YYYY-MM-DD"))
+        
+        if date2 > date1 {
+            
+            // TODO something
+            print("Hello")
+
+        }
+        
+        
+		let abb = d.toRelativeString(abbreviated: true, maxUnits: 3)
+		print("data: \(abb)")
 //		
 //		let w = NSDate()-1.month
 //		print("Prev month: \(w)")

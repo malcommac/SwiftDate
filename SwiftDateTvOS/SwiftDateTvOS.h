430813365b877a26b12fe2c3067e661736c25feb
Daniele Margutti <hello@danielemargutti.com>
2015-12-21 22:54:55 +0100

    - [NEW] Added TvOS, WatchOS and OSX shared scheme for carthage

---
+++ SwiftDateTvOS/SwiftDateTvOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDateTvOS.h
+//  SwiftDateTvOS
+//
+//  Created by Daniele Margutti on 21/12/15.
+//  Copyright © 2015 Daniele Margutti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDateTvOS.
+FOUNDATION_EXPORT double SwiftDateTvOSVersionNumber;
+
+//! Project version string for SwiftDateTvOS.
+FOUNDATION_EXPORT const unsigned char SwiftDateTvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDateTvOS/PublicHeader.h>
+
+

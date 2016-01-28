bc5d64d34fe6ab6b8d63fd5fc4f2f57c7297c32f
daniele margutti <hello@danielemargutti.com>
2016-01-28 19:15:33 +0100

    - Added support to build framework on watchos/ios/osx/tvos

--- SwiftDate/SwiftDate/SwiftDate.h
+++ SwiftDate/SwiftDate/SwiftDate.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 breakfastcode. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for SwiftDate.
 FOUNDATION_EXPORT double SwiftDateVersionNumber;

5b53c40d7f738ff76468d502f89fe0ceff959964
daniele margutti <hello@danielemargutti.com>
2016-03-29 23:56:28 +0200

    Added (empty) DateFormatterTests class

---
+++ Sources/SwiftDateTests/DateFormatterTests.swift
@@ -0,0 +1,24 @@
+//
+//  DateInRegionComponentPortTests.swift
+//  DateInRegion
+//
+//  Created by Jeroen Houtzager on 07/11/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+// https://github.com/Quick/Quick
+
+
+import Quick
+import Nimble
+import SwiftDate
+
+class DateFormatterTests: QuickSpec {
+
+ 	override func spec() {
+		describe("DateFormatterTests") {
+		
+				
+		}
+	}
+	
+}

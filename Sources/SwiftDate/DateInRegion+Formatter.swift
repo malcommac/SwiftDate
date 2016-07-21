63d9d41c556a314c95933217d2fa70085be91f71
Daniele Margutti <hello@danielemargutti.com>
2016-07-21 19:26:33 +0200

    When parsing ISO strings, DateInRegion correctly sets locale to POSIX. However, when writing ISO strings it does not set the locale to POSIX, resulting in the wrong format on some devices. Thanks to @phatmann

--- Sources/SwiftDate/DateInRegion+Formatter.swift
+++ Sources/SwiftDate/DateInRegion+Formatter.swift
@@ -121,7 +121,12 @@ public extension DateInRegion {
 			cachedFormatter.dateFormat = dateFormatString
 			cachedFormatter.timeZone = self.region.timeZone
 			cachedFormatter.calendar = self.region.calendar
-			cachedFormatter.locale = self.region.calendar.locale
+			switch dateFormat {
+			case .ISO8601, .ISO8601Format:
+				cachedFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+			default:
+				cachedFormatter.locale = self.region.calendar.locale
+			}
 			let value = cachedFormatter.stringFromDate(self.absoluteTime!)
 			return value
 		}

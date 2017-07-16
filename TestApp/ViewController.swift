79feaf8832165065430c446f3034443858f0d58a
Daniele Margutti <hello@danielemargutti.com>
2017-07-16 17:19:04 +0200

    ISO8601 Parser does not keep milliseconds info

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -14,23 +14,6 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		
-
-		let gmt = Region(tz: TimeZoneName.gmt, cal: CalendarName.gregorian, loc: LocaleName.english)
-		let startDate = DateInRegion(string: "2017-07-16 16:00:00 +0000", format: .iso8601Auto, fromRegion: gmt)!
-		var nextWeek = startDate.prevMonth(at: .start)
-		for _ in 0..<10 {
-			nextWeek = nextWeek.nextMonth(at: .start)
-			print(nextWeek.string(format: .iso8601Auto))
-		}
-
-
-		
-		/*var currentEndMonth = endMonth.prevMonth.endOf(component: .month)
-		for _ in 0..<10 {
-			currentEndMonth = currentEndMonth.nextMonth
-			print(currentEndMonth)
-		}*/
 	}
 
 	override func didReceiveMemoryWarning() {

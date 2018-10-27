feb037fe9b1103b20c06ae947c938ca8082fc5a7
Daniele Margutti <hello@danielemargutti.com>
2018-10-27 20:50:47 +0200

    #568 Workaround for tests on DateComponentsFormatter.ZeroFormattingBehavior under linux

--- Tests/SwiftDateTests/TestFormatters.swift
+++ Tests/SwiftDateTests/TestFormatters.swift
@@ -452,8 +452,14 @@ class TestFormatters: XCTestCase {
 	func testTimeInterval_Clock() {
 		let value = (2.hours + 5.minutes).timeInterval.toClock()
 		XCTAssert(value == "2:05:00", "Failed to format clock")
+		#if os(Linux)
+		let zeroBehavior = DateComponentsFormatter.ZeroFormattingBehavior(rawValue: 14)
+		let value2 = (4.minutes + 50.minutes).timeInterval.toClock(zero: zeroBehavior)
+		XCTAssert(value2 == "54:00", "Failed to format clock")
+		#else
 		let value2 = (4.minutes + 50.minutes).timeInterval.toClock(zero: DateComponentsFormatter.ZeroFormattingBehavior.dropAll)
 		XCTAssert(value2 == "54:00", "Failed to format clock")
+		#endif
 	}
 
 	func testFormatterCustom() {

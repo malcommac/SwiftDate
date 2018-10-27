eadbd1373129bd69d5345b1f1013c9ff68e4d890
Daniele Margutti <hello@danielemargutti.com>
2018-10-27 20:43:38 +0200

    #568 Removed arc4random in tests

--- Tests/SwiftDateTests/TestDateInRegion+Create.swift
+++ Tests/SwiftDateTests/TestDateInRegion+Create.swift
@@ -11,7 +11,7 @@ import XCTest
 
 public func randomNumber<T: SignedInteger>(inRange range: ClosedRange<T> = 1...6) -> T {
 	let length = Int64(range.upperBound - range.lowerBound + 1)
-	let value = Int64(arc4random()) % length + Int64(range.lowerBound)
+	let value = Int64.random(in: 0..<Int64.max) % length + Int64(range.lowerBound)
 	return T(value)
 }
 

ae2893b33f059c433ebd5c225ace7992c8f7f0f4
Roman Podymov <podymfrombryansk@yandex.ru>
2019-09-14 16:57:15 +0200

    testPositionInRange

--- Tests/SwiftDateTests/TestDate.swift
+++ Tests/SwiftDateTests/TestDate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 SwiftDate. All rights reserved.
 //
 
+import SwiftDate
 import XCTest
 
 class TestDate: XCTestCase {
@@ -32,4 +33,24 @@ class TestDate: XCTestCase {
         print(result!)
     }
 
+    func testPositionInRange() {
+        let regionRome = Region(calendar: Calendars.gregorian, zone: Zones.europeRome, locale: Locales.italian)
+        let regionLondon = Region(calendar: Calendars.gregorian, zone: Zones.europeLondon, locale: Locales.english)
+        let dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let lowerBound = "2018-05-31 23:00:00".toDate(dateFormat, region: regionRome)!.date
+        let upperBound = "2018-06-01 01:00:00".toDate(dateFormat, region: regionRome)!.date
+        
+        let testDate1 = "2018-06-01 00:30:00".toDate(dateFormat, region: regionRome)!.date
+        XCTAssertEqual( testDate1.positionInRange(date: lowerBound, and: upperBound), 0.75)
+        
+        let testDate2 = "2018-05-31 22:30:00".toDate(dateFormat, region: regionLondon)!.date
+        XCTAssertEqual( testDate2.positionInRange(date: lowerBound, and: upperBound), 0.25)
+        
+        let testDate3 = "2018-05-31 21:00:00".toDate(dateFormat, region: regionLondon)!.date
+        XCTAssertNil( testDate3.positionInRange(date: lowerBound, and: upperBound))
+        
+        let testDate4 = "2018-06-01 03:00:00".toDate(dateFormat, region: regionLondon)!.date
+        XCTAssertNil( testDate4.positionInRange(date: lowerBound, and: upperBound))
+    }
 }

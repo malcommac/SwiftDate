ca41279a6e032457ea688e631bee8311b401cac3
Brendan Kilfoil <brendan.kilfoil@finger-ink.com>
2019-11-29 09:25:35 +1300

    Respect formatting

--- Sources/SwiftDate/Formatters/RelativeFormatter/RelativeFormatterLanguage.swift
+++ Sources/SwiftDate/Formatters/RelativeFormatter/RelativeFormatterLanguage.swift
@@ -19,8 +19,8 @@ internal class RelativeFormatterLanguagesCache {
     private(set) var cachedValues = [String: [String: Any]]()
 
     func flavoursForLocaleID(_ langID: String) -> [String: Any]? {
-		if let cachedFlavour = cachedValues[langID] {
-			return cachedFlavour
+        if let cachedFlavour = cachedValues[langID] {
+            return cachedFlavour
 		}
         do {
             guard let fullURL = Bundle(for: RelativeFormatter.self).resourceURL?.appendingPathComponent("langs/\(langID).json") else {

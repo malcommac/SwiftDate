12367b6c9af7e6468fc8d453c292c0cec7bd2ee6
Samuel Spencer <Sam-Spencer@users.noreply.github.com>
2020-09-21 16:48:18 -0500

    Update Package.swift
    Bumped minimum iOS deployment version from 8 to 9 in order to comply with Xcode 12's minimum deployment range. Fixes compiler warning.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftDate",
     platforms: [
-        .macOS(.v10_10), .iOS(.v8), .watchOS(.v2), .tvOS(.v9)
+        .macOS(.v10_10), .iOS(.v9), .watchOS(.v2), .tvOS(.v9)
     ],
     products: [
         .library(
